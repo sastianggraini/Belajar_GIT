0d7c46c00fea589f528733abd0ab256516f63a7d
sastianggraini <160709652+sastianggraini@users.noreply.github.com>
2024-02-28 15:01:41 +0700

    update profil
    -menambah profil

---
+++ resources/views/profil.blade.php
@@ -0,0 +1,1 @@
+<h1>Sasti Anggraini</h1>
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/', function () {
     return view('login');
 });
+
+Route::get('/', function () {
+    return view('profil');
+});
